c6ae19685eca0833b36252a8baac80031a4c73ff
megamiSoh <sej@whybe.co.kr>
2020-09-30 01:04:00 +0900

    response로 path전달로 변경 / 이미지 미리보기 삭제

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const fileUpload = require("express-fileupload");
 const potrace = require("potrace");
 
 const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true,
+    origin: "http://localhost:3000",
+    credentials: true,
 };
 
 server.use(cors(corsOptions));
@@ -16,10 +16,10 @@ server.use(cors(corsOptions));
 server.use(express.static(`${__dirname}/public/tmp`));
 
 server.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/tmp/",
-  })
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+    })
 );
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
@@ -27,67 +27,43 @@ server.use(express.urlencoded({ extended: true }));
 // express fileupload 라이브러리를 사용
 // 사용자가 보낸 이미지를 임시 디렉토리에 저장한다.
 server.post("/sendImage", (req, res) => {
-  // 파일이 없으면 status 400을 리턴한다.
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400);
-  }
-  let uploadImg = req.files;
-  let uploadImgName = req.files.image.name;
-  let len = uploadImgName.length;
-
-  // mv method를 이용하여 이미지 파일을 임시 저장한당.
-  uploadImg.image.mv(
-    `${__dirname}/tmp/megamiSoh.${uploadImgName.substr(len - 3)}`,
-    function (err) {
-      if (err) return res.status(500).send(err);
-      res.send({ msg: "upload complete" });
+    // 파일이 없으면 status 400을 리턴한다.
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400);
     }
-  );
+    let uploadImg = req.files;
+    let uploadImgName = req.files.image.name;
+    let len = uploadImgName.length;
+
+    // mv method를 이용하여 이미지 파일을 임시 저장한당.
+    uploadImg.image.mv(
+        `${__dirname}/tmp/megamiSoh.${uploadImgName.substr(len - 3)}`,
+        function(err) {
+            if (err) return res.status(500).send(err);
+            res.send({ msg: "upload complete" });
+        }
+    );
 });
 
-// 스트림에 업로드한 이미지 주소를 전달 받는다.
-function imageTransform(path) {
-  const readStream = fs.createReadStream(path);
-  return readStream;
-}
 
 // 이미지의 파라미터 값을 받는다.
 server.post("/getAttr", (req, res) => {
-  let default_setting = {
-    color: `lightgray`,
-    optTolerance: 0.4,
-    turdSize: 100,
-    turnPolicy: "majority",
-  };
-  // 파라미터값을 potrace에 전달하여 이미지를 변경한다.
-  const isEmpty = (param) => {
-    return Object.keys(param).length === 0;
-  };
-  let params = isEmpty(req.body) ? default_setting : req.body;
-  let trace = new potrace.Potrace(params);
-  trace.loadImage(`${__dirname}/tmp/megamiSoh.png`, function (err) {
-    if (err) throw err;
-    let getSvgFile = trace.getSVG();
-    res.send({ image: getSvgFile });
-  });
+    // 파라미터값을 potrace에 전달하여 이미지를 변경한다.
+    const isEmpty = (param) => {
+        return Object.keys(param).length === 0;
+    };
+    let params = isEmpty(req.body) ? default_setting : req.body;
+    let trace = new potrace.Potrace(params);
+    trace.loadImage(`${__dirname}/tmp/megamiSoh.png`, function(err) {
+        if (err) throw err;
+        let getSvgFile = trace.getSVG();
+        res.send({ image: getSvgFile });
+    });
 });
 
-// 이미지 미리보기
-server.get("/", (req, res) => {
-  const format = req.query.format;
-  // if (format === "svg") {
-  //   res.type("image/svg+xml");
-  // } else {
-  //   res.type(`image/${format || "png"}`);
-  // }
-  res.type("image/svg+xml");
-  // imageTransform(`${__dirname}/tmp/megamiSoh.${format || "png"}`).pipe(res);
-  imageTransform(`${__dirname}/tmp/megamiSoh.svg`).pipe(res);
-  res.send(imageTransform(`${__dirname}/tmp/megamiSoh.svg`));
-});
 
 server.listen(8005, () => {
-  console.log("Server started!");
+    console.log("Server started!");
 });
 
 // 클라이언트에서 사용자가 이미지 파일을 업로드
